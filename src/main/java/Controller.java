f7a07012edcb48b238807c77758a88665faa2773
mapopescu5079 <popescu.matthew@gmail.com>
2020-09-03 22:24:34 -0400

    updated sample.fxml

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,15 +1,28 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 
 public class Controller {
 
     @FXML
-    private Label lblOutput;
+    private Button btnAddProduct;
 
-    public void sayHello() {
-        lblOutput.setText("Hello FXML!");
+    @FXML
+    private Button btnRecordProduction;
 
+    @FXML
+    private ComboBox<?> comboBox;
+
+    @FXML
+    void display(ActionEvent event) {
+        System.out.println(btnAddProduct);
     }
 
-}
+    @FXML
+    void rpDisplay(ActionEvent event) {
+        System.out.println(btnRecordProduction);
+    }
 
+
+}
