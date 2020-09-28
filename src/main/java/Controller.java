5ca5967947714bf5e9f3d627a29498d0237d11dc
mapopescu5079 <popescu.matthew@gmail.com>
2020-09-27 22:35:13 -0400

    Activity 3 complete

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,6 +1,8 @@
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -11,55 +13,34 @@ import java.util.Scanner;
 
 public class Controller {
 
+    //Product Line tab
     @FXML
     private TextField tfProductName;
-    public TextField getTfProductName() {
-        return tfProductName;
-    }
-
-    public void setTfProductName(TextField tfProductName) {
-        this.tfProductName = tfProductName;
-    }
 
     @FXML
     private TextField tfManufacturer;
-    public TextField getTfManufacturer() {
-        return tfManufacturer;
-    }
-
-    public void setTfManufacturer(TextField tfManufacturer) {
-        this.tfManufacturer = tfManufacturer;
-    }
 
     @FXML
-    private TextField tfItem;
-    public TextField getTfItem() {
-        return tfItem;
+    private ChoiceBox<String> chbItem;
+
+    @FXML
+    private Button btnAddProduct;
+
+    @FXML
+    void btnAddProduct(ActionEvent event) {
+        connectToDb();
     }
 
-    public void setTfItem(TextField tfItem) {
-        this.tfItem = tfItem;
-    }
+    //Produce tab
+    @FXML
+    private Button btnRecordProduction;
+
+    @FXML
+    void btnRecordProduction(ActionEvent event) { }
 
     @FXML
     private ComboBox<String> cmbQuantity;
 
-    @FXML
-    void btnAddProduct() {
-      //  connectToDb();
-    }
-
-    @FXML
-    void showProduct(ActionEvent event) {
-        connectToDb();
-    }
-
-    @FXML
-    void btnRecordProduction(MouseEvent event) {
-        System.out.println("Production Recorded.");
-    }
-
-    // void showDetails(ActionEvent event){connectToDb();}
 
     public void initialize(){
         for (int count = 1; count <= 10; count++){
@@ -67,14 +48,19 @@ public class Controller {
         }
         cmbQuantity.setEditable(true);
         cmbQuantity.getSelectionModel().selectFirst();
-    }
 
+        chbItem.getItems().add("ipod2");
+        chbItem.getItems().add("iwatch");
+        chbItem.getItems().add("irobot");
+    }
 
 
     public void connectToDb(){
 
         final String JDBC_DRIVER = "org.h2.Driver";
         final String DB_URL = "jdbc:h2:./res/resDB";
+        System.out.println("connected to database");
+
 
         //  Database credentials
         final String USER = "";
@@ -82,17 +68,12 @@ public class Controller {
         Connection conn = null;
         Statement stmt = null;
 
-        String make = "";
-        String manufacturer = "";
-        String name = "";
+        String productName = tfProductName.getText();
+        String manufacturer = tfManufacturer.getText();
+        String item = chbItem.getValue();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("enter type: ");
-        make = scanner.next();
-        System.out.println("enter manufacturer: ");
-        manufacturer=scanner.next();
-        System.out.println("enter name: ");
-        name = scanner.next();
+        System.out.println(productName + " " + manufacturer + " " + item);
+
 
         try {
 
@@ -105,15 +86,14 @@ public class Controller {
             System.out.println("Connection Successful");
 
             //STEP 3: Execute a query
-            System.out.println("attempting to insert");
+
             final String sql = "INSERT INTO Product (type, manufacturer, name)"
-                    + "VALUES (?,?,?)";
+                    + "VALUES (?, ?, ?)";
 
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, make);
+            ps.setString(1, productName);
             ps.setString(2, manufacturer);
-            ps.setString(3, name);
-
+            ps.setString(3, item);
             ps.executeUpdate();
             System.out.println("insert successful");
 
@@ -121,10 +101,12 @@ public class Controller {
 
             String showSql = "SELECT * FROM Product";
 
+
             ResultSet rs = stmt.executeQuery(showSql);
             while (rs.next()) {
                 System.out.println(rs.getString(2));
             }
+
             // STEP 4: Clean-up environment
              stmt.close();
              conn.close();
@@ -135,8 +117,8 @@ public class Controller {
             e.printStackTrace();
         }
         // insert PRODUCT table here!!!
-        System.out.println("Products Successfully inserted into table");
-    } // end connectToDb
+        System.out.println("done");
 
+    } // end connectToDb
 } // end Controller
 
